ae96b4b127bd5e8ec095ae70e7b6ae62ed1b0414
fazzy12 <kaluifeanyi00@gmail.com>
2023-04-09 20:31:28 +0100

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H. Your text editor
+can execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the fewest number
+of operations needed to result in exactly n H characters in the file.
+    Prototype: def minOperations(n)
+    Returns an integer
+    If n is impossible to achieve, return 0
+Example:
+
+
+
+
+//////////////////////////////////////////////////
+n = 9
+
+H =>
+Copy All => Paste => HH =>
+Paste =>HHH =>
+Copy All => Paste => HHHHHH =>
+Paste => HHHHHHHHH
+
+Number of operations: 6
+"""
+
+
+def minOperations(n):
+    amount = 0
+    minimum = 2
+    while n > 1:
+        while n % minimum == 0:
+            amount += minimum
+            n /= minimum
+        minimum += 1
+    return amount
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,7 @@
+<p align="center">
+    <a href=#><img src="https://raw.githubusercontent.com/jbocane6/logos/main/holberton-logo.png" alt="holberton" /></a></p>
+  
+  <p align="center">
+    <a href="https://twitter.com/juanoso07555284" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="juanoso07555284" height="30" width="40" /></a>
+  <a href="https://linkedin.com/in/juan-camilo-bocanegra-osorio-18b1821a6" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="juan-camilo-bocanegra-osorio-18b1821a6" height="30" width="40" /></a>
+  </p>
